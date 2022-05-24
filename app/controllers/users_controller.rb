d9e21931d49ff8221a3f992e6701d916c1f334ab
Joseph Senecal <jasenecal2@gmail.com>
2022-05-23 23:04:11 -0600

    Ref: new user form and global flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user = User.create!(user_params)
-    redirect_to "/users/#{new_user.id}/"
+    new_user = User.new(user_params)
+    if new_user.save
+      redirect_to "/users/#{new_user.id}/"
+    else
+      flash[:error] = new_user.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
   def show
@@ -15,6 +20,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.permit(:name, :email, :password)
+      params.permit(:name, :email, :password, :password_confirmation)
     end
 end
