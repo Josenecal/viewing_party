7e6ddd706ea891c93b09a3733d666cc5086268a6
Joseph Senecal <jasenecal2@gmail.com>
2022-05-24 15:48:58 -0600

    Ref: landing page no longer links to all users' dashboards

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe "landing page" do
     visit '/'
 
     expect(page).to_not have_content("Carl")
-    expect(page).to have_button("Create New User")
+    expect(page).to have_button("Create An Account")
 
-    click_button "Create New User"
+    click_button "Create An Account"
     fill_in "Name", with: "Carl"
     fill_in "Email", with: "carl@catmail.com"
     fill_in :password, with: "pAsSwOrD"
@@ -24,18 +24,22 @@ RSpec.describe "landing page" do
     expect(page).to have_content("Carl")
   end
 
-  it "has a list of existing users that link to their dashboards" do
+  it "has a button to login" do
     user_1 = User.create!(name: "Twitch", email: "twitch@dogmail.com", password: "test", password_confirmation: "test")
     user_2 = User.create!(name: "Riley", email: "riley@dogmail.com", password: "this_is_a_password", password_confirmation: "this_is_a_password")
     user_3 = User.create!(name: "Carl", email: "carl@catmail.com", password: "password", password_confirmation: "password")
     visit '/'
 
-    expect(page).to have_content("Twitch")
-    expect(page).to have_content("Riley")
-    expect(page).to have_content("Carl")
+    click_on "Login"
 
-    click_link "Riley"
-    expect(current_path).to eq("/users/")
+    fill_in :email, with: user_1.email
+    fill_in :password, with: user_1.password
+    click_on "Login"
+
+    expect(page).to have_content("Twitch")
+    expect(page).not_to have_content("Riley")
+    expect(page).not_to have_content("Carl")
+
   end
 
   it "has a link to the landing page" do
