106ef7bfb4126fdceb489b4bab2681bd0c00649e
Joseph Senecal <jasenecal2@gmail.com>
2022-05-24 14:32:42 -0600

    Ref: add user ID to session upon successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
-      # session[:user_id] = user.id
+      session[:user_id] = user.id
       flash[:success] = "Welcome back, #{user.name.capitalize}"
       redirect_to "/users/#{user.id}/"
     else
